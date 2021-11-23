77a7e824e190921425dd47c458120a0f6da6bd9e
Florea Eric <ericflaviu.florea@gmail.com>
2021-11-23 21:44:44 +0200

    added derived classes for mouse and cats from animal

--- CSharpOOp/CSharpOOp/FloreaEric.cs
+++ CSharpOOp/CSharpOOp/FloreaEric.cs
@@ -5,21 +5,64 @@ namespace CSharpOOp
     //public, private -> access modifiers
     public class Animal
     {
-        private string name; //atribut / field
+	    private string name; //atribut / field
 
-		public Animal()
-		{
-			Console.WriteLine("Create animal");
-		}
+	    public Animal()
+	    {
+		    Console.WriteLine("Create animal");
+	    }
 
-		public Animal(string name)
-		{
-			this.name = name;
-		}
+	    public Animal(string name)
+	    {
+		    this.name = name;
+	    }
+	    // functie = primeste un numar de argumente si returneaza ceva inapoi
+	    // metoda = acelasi lucru cu functia dar putem accesa orice variabila
 
-        public string GetName()
-		{
-			return name;
-		}
+	    //Getter
+	    //virtual -> metoda poate fi suprascris in clasa derivata
+	    public virtual string GetName()
+	    {
+		    if (name.Contains("o"))
+		    {
+			    name = name.Replace(oldValue: "o", newValue: "a");
+		    }
+
+		    return name;
+	    }
+
+	    //Setter
+	    public void SetName(string name)
+	    {
+		    this.name = name.ToLower();
+	    }
+
+	    
+	    
+}
+    //mostenire
+    //clasa derivata
+    public class Cat : Animal
+    {
+	    public Cat(string name) : base(name){}
+
+	    public override string GetName()
+	    {
+		    var baseName = base.GetName();
+		    return baseName + " The Cat";
+	    }
+		    
+    }
+
+    public class Mouse : Animal
+    {
+	    public Mouse(string name) : base(name){}
+
+	    public override string GetName()
+	    {
+		    var baseName = base.GetName();
+		    return baseName + " The Mouse";
+	    }
+		    
     }
 }
--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -7,8 +7,19 @@ namespace CSharpOOp
         static void Main(string[] args)
         {
             // animal este o instanta a clasei Animal
-            Animal animal = new Animal();
-            Console.WriteLine(animal.GetName());
+            Cat cat = new Cat(name:"Tom");
+            Mouse mouse = new Mouse(name: "Jerry");
+            
+            Console.WriteLine(cat.GetName());
+            cat.SetName("Another Tom");
+            //Console.WriteLine(animal.GetName());
+            Console.WriteLine(mouse.GetName());
+            
+            // OOP building block;
+            // polimorfism
+            // mostenire // 
+            // incapsulare
+            // abstractizare
         }
     }
 }

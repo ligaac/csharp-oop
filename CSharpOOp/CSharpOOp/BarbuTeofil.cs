a5ba95373a3131d3832fa8ce55a8830de49a9aaa
Teofil Barbu <teofil.barbu2@student.upt.ro>
2021-11-23 21:58:15 +0200

    Finding obj type

--- CSharpOOp/CSharpOOp/BarbuTeofil.cs
+++ CSharpOOp/CSharpOOp/BarbuTeofil.cs
@@ -5,6 +5,7 @@ namespace CSharpOOp
     public class Animal
     {
         public string name;
+        public string type;
 
         public Animal()
         {
--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -13,6 +13,25 @@ namespace CSharpOOp
            
             Console.WriteLine(cat.GetName());
             Console.WriteLine(mouse.GetName());
+
+            var animal1 = cat as Animal;
+            var animal2 = mouse as Animal;
+            Console.WriteLine(animal1.GetName()); //"Tam the cat"
+
+            Animal[] myList = new[] {animal1, animal2};
+
+            foreach (var animal in myList)
+            {
+                if (animal.GetType() == typeof(Cat))
+                {
+                    Console.WriteLine("Is Cat");
+                }
+                else
+                {
+                    Console.WriteLine("Is Mouse");
+                }
+                
+            }
         }
     }
 }

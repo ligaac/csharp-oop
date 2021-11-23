2a9920db6366806439ed5215c47d00e117d9337d
lunguleasa eugen <11alexandru12@gmail.com>
2021-11-23 20:49:47 +0200

    Am adaugat modificari

--- CSharpOOp/CSharpOOp/Eugen.cs
+++ CSharpOOp/CSharpOOp/Eugen.cs
@@ -1,7 +1,25 @@
-﻿namespace CSharpOOp
+﻿using System;
+
+namespace CSharpOOp
 {
-    public class Eugen
+    public class Animal
     {
-        
+        //  atribut/fiel
+        private string name;
+
+        public Animal()
+        {
+            Console.WriteLine("Create animal");
+        }
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
     }
 }
--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -6,7 +6,14 @@ namespace CSharpOOp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int , float , char ,etc -> tipuri primitive
+            int x = 3;
+            
+            //animal e o instanta a clasei Animal
+            Animal animal = new Animal("Tom");
+            Animal mouse = new Animal("Jerry");
+            Console.WriteLine(animal.GetName());
+            Console.WriteLine(mouse.GetName());
         }
     }
 }

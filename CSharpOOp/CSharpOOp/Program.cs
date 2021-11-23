cef02b61790f671071bc3d3493e9b76ea3f20fa3
Victor TOPORAN <victor.toporan@ligaac.com>
2021-11-23 20:51:37 +0200

    Adds basic Animal class

--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -6,7 +6,14 @@ namespace CSharpOOp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int, char, float -> primitive;
+            int a = 3;
+
+            // animal, mouse -> instances
+            Animal animal = new Animal("AnimalName");
+            Animal mouse = new Animal("MouseName");
+            Console.WriteLine(animal.getName());
+            Console.WriteLine(mouse.getName());
         }
     }
 }
--- CSharpOOp/CSharpOOp/VictorToporan.cs
+++ CSharpOOp/CSharpOOp/VictorToporan.cs
@@ -1,7 +1,19 @@
-namespace a
+namespace CSharpOOp
 {
-    public class VictorToporan
+    // public, private -> access modifier
+    public class Animal
     {
-        
+        // atribut / filed
+        public string name;
+
+        public Animal(string name) // constructor
+        {
+            this.name = name;
+        }
+
+        public string getName() // getter
+        {
+            return this.name;
+        }
     }
 }

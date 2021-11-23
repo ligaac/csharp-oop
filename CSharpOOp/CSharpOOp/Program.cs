5df5672760097e6e768c86c897e4449b13322a20
AlexVF <fugata.alexandru@gmail.com>
2021-11-23 21:58:37 +0200

    added polimorphism

--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -11,6 +11,21 @@ namespace CSharpOOp
             //animal este o instanta a clasei Animal
             Cat cat = new Cat(name:"Tom");
             Mouse mouse = new Mouse(name: "Jerry");
+            var animal1 = cat as Animal;
+            var animal2 = mouse as Animal;
+            Animal[] myList = new[] {animal1, animal2};
+            foreach (var animal in myList)
+            {   if(animal is Cat)
+                //if (animal.GetType() == typeof(Cat))
+                {
+                    Console.WriteLine("IS CAT");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("is mouse");
+                }
+            }
             Console.WriteLine(cat.GetName());
             //cat.SetName("Another Tom");
           //  Console.WriteLine(cat.GetName());

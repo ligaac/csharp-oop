b7b690c7429f499d0960066de4b4ab59abdc5683
MadalinP <unicornshovel@gmail.com>
2021-11-23 20:49:45 +0200

    class animal

--- CSharpOOp/CSharpOOp/Madalin.cs
+++ CSharpOOp/CSharpOOp/Madalin.cs
@@ -1,8 +1,25 @@
-﻿namespace CsharpOOp
-{
-    public class Madalin
-    {
+﻿using System;
 
+namespace CsharpOOp
+{
+    //public,private ->access modifiers
+    public class Animal
+    {
+        private string name;
+
+        public Animal()
+                 {
+                     Console.WriteLine("Create animal");
+                 }
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        
     }
 
 }
--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CsharpOOp;
 
 namespace CSharpOOp
 {
@@ -6,7 +7,16 @@ namespace CSharpOOp
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
+            //int,float,char ->date primitve 
+            int x = 3;
+            //animal este o instanta a clasei Animal
+            Animal animal = new Animal();
+            Animal mouse = new Animal("Jerry");
+            Console.WriteLine(animal.GetName());
+            Console.WriteLine(mouse.GetName());
+
         }
     }
 }

84e572dc73a0cb3364b6ea6f505bb0509f7f35df
Dan Percic <dan.percic@ligaac.ro>
2021-11-23 20:49:42 +0200

    created class Animal and some constructors

--- CSharpOOp/CSharpOOp/DanPercic.cs
+++ CSharpOOp/CSharpOOp/DanPercic.cs
@@ -1,7 +1,26 @@
+using System;
+
 namespace CSharpOOp
 {
-    public class DanPercic
+    // public, private -> access modifiers
+    public class Animal
     {
-        
+        // atribut / field
+        private string name;
+
+        public Animal()
+        {
+            Console.WriteLine("Create animal");
+        }
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
     }
 }
--- CSharpOOp/CSharpOOp/Program.cs
+++ CSharpOOp/CSharpOOp/Program.cs
@@ -6,7 +6,14 @@ namespace CSharpOOp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int, float, char, etc, etc -> tipuri primitive;
+            int x = 3;
+
+            // animal este o instanță a clasei Animal
+            Animal animal = new Animal("Tom");
+            Animal mouse = new Animal("Jerry");
+            Console.WriteLine(animal.GetName());
+            Console.WriteLine(mouse.GetName());
         }
     }
 }
